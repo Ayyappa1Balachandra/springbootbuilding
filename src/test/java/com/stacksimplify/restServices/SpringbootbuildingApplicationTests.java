860a698893c0ee18e7d437edb4cc3802960c578d
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2025-10-28 18:14:43 +0530

    First Commit

---
+++ src/test/java/com/stacksimplify/restServices/SpringbootbuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
